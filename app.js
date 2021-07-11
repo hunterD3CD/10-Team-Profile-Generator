b5ed1ebb4a469316fa64e102339bffa64bf5d82d
hunterD3CD <hunterd3c@gmail.com>
2021-07-11 14:47:05 -0700

    capture user input for employees: engineer and intern

--- app.js
+++ app.js
@@ -6,8 +6,13 @@ const inquirer = require("inquirer");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+const Employee = require("./lib/Employee");
 
-// ------------------------------- CAPTURE USER INPUT (Inquirer module): Manager, Engineer, Intern --------------------------------
+var intern;
+var manager;
+var engineer;
+
+// ------------------------------- CAPTURE USER INPUT (Inquirer module): Manager  --------------------------------
 function managerInfo() {
   inquirer
     .prompt([
@@ -40,7 +45,81 @@ function managerInfo() {
         managerInput.managerEmail,
         managerInput.officeNumber
       );
+      console.log(
+        "after entering manager info, please fill in employee's info"
+      );
       console.log(manager);
+
+      employeeInfo();
     });
 }
 managerInfo();
+
+// ------------------------------- CAPTURE USER INPUT (Inquirer module): Engineer, Intern --------------------------------
+function employeeInfo() {
+  inquirer
+    .prompt([
+      {
+        name: "employeeRole",
+        type: "list",
+        message: "What is the role of your employee?",
+        choices: ["Engineer", "Intern"],
+      },
+      {
+        name: "employeeName",
+        type: "input",
+        message: "What is employee's name?",
+      },
+      {
+        name: "employeeId",
+        type: "input",
+        message: "What is the employee ID?",
+      },
+      {
+        name: "employeeEmail",
+        type: "input",
+        message: "What is the employee's email?",
+      },
+      {
+        name: "engineerGithub",
+        type: "input",
+        message: "What is the Github?",
+        when: (userInput) => userInput.employeeRole === "Engineer",
+      },
+      {
+        name: "internSchool",
+        type: "input",
+        message: "What is the school?",
+        when: (userInput) => userInput.employeeRole === "Intern",
+      },
+      {
+        name: "addEmployee",
+        type: "confirm",
+        message: "would you add new employee?",
+      },
+    ])
+    .then((employeeInput) => {
+      if (employeeInput.employeeRole === "Engineer") {
+        let engineer = new Engineer(
+          employeeInput.employeeName,
+          employeeInput.employeeId,
+          employeeInput.employeeEmail,
+          employeeInput.engineerGithub
+        );
+        console.log(engineer);
+      } else if (employeeInput.employeeRole === "Intern") {
+        let intern = new Intern(
+          employeeInput.employeeName,
+          employeeInput.employeeId,
+          employeeInput.employeeEmail,
+          employeeInput.internSchool
+        );
+        console.log(intern);
+      }
+      if (employeeInput.addEmployee === true) {
+        employeeInfo();
+      } else {
+
+      }
+    });
+}
